f79a0c6e1cad3fd271722232429a96e69e7ef1c1
Eloise Holland <eloise.holland@Mac.Home>
2025-03-13 15:09:57 +0000

    add create method to application controller

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -6,4 +6,5 @@ import javax.inject._
 @Singleton
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   def index() = TODO
+  def create() = TODO
 }
