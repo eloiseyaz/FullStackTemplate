573b698886919d77796b109441f400d8d9df60d6
Eloise Holland <eloise.holland@Mac.Home>
2025-03-13 14:59:26 +0000

    create application controller class with index

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,9 @@
+package controllers
+
+import play.api.mvc._
+import javax.inject._
+
+@Singleton
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  def index() = TODO
+}
