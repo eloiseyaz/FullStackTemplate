570e269474acc71d11618c083ed5f6a6d57171e7
Eloise Holland <eloise.holland@Mac.Home>
2025-03-13 15:52:54 +0000

    write index function with TDD

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,12 +1,14 @@
 package controllers
 
 import play.api.mvc._
+
 import javax.inject._
+import scala.concurrent.Future
 
 @Singleton
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def index() = TODO
+  def index(): Action[AnyContent] = Action{Ok}
 
   def create() = TODO
 
--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,6 +1,9 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import play.api.test.FakeRequest
+import play.api.http.Status
+import play.api.test.Helpers._
 
 class ApplicationControllerSpec extends BaseSpecWithApplication {
 
@@ -9,6 +12,12 @@ class ApplicationControllerSpec extends BaseSpecWithApplication {
   )
 
   "ApplicationController .index" should {
+
+    val result = TestApplicationController.index()(FakeRequest())
+
+    "return 200 OK" in {
+      status(result) shouldBe Status.OK
+    }
 
   }
 
