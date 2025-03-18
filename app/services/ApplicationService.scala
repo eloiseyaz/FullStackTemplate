6f0a9abe92b45b954efb9945d0bc9babe222ca54
Eloise Holland <eloise.holland@Mac.Home>
2025-03-18 11:57:00 +0000

    add imports for ApplicationService

--- app/services/ApplicationService.scala
+++ app/services/ApplicationService.scala
@@ -1,10 +1,17 @@
 package services
 
+import connectors.LibraryConnector
+import models.DataModel
+import play.api.libs.json.{Json, OFormat}
+
+import javax.inject._
+import scala.concurrent.{ExecutionContext, Future}
+
 @Singleton
 class LibraryService @Inject()(connector: LibraryConnector) {
 
   def getGoogleBook(urlOverride: Option[String] = None, search: String, term: String)(implicit ec: ExecutionContext): Future[Book] =
-    connector.get[DataModel](urlOverride.getOrElse(s"https://www.googleapis.com/books/v1/volumes?q=$search%$term"))
+    connector.get[DataModel](urlOverride.getOrElse(s"https://www.googleapis.com/books/v1/volumes?q=$search%$term"))//.map{DataModel => Book.}
 
 }
 
