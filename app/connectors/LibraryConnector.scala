23659efa6f82ead9919c4380ed788e7d4a4234d5
Eloise Holland <eloise.holland@Mac.Home>
2025-03-18 11:46:13 +0000

    add imports for LibraryConnector

--- app/connectors/LibraryConnector.scala
+++ app/connectors/LibraryConnector.scala
@@ -1,5 +1,11 @@
 package connectors
 
+import play.api.libs.json.OFormat
+import play.api.libs.ws.WSClient
+
+import javax.inject._
+import scala.concurrent.{ExecutionContext, Future}
+
 class LibraryConnector @Inject()(ws: WSClient) {
   def get[Response](url: String)(implicit rds: OFormat[Response], ec: ExecutionContext): Future[Response] = {
     val request = ws.url(url)
