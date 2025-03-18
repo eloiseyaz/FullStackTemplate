f133da7305a471cdcab72b2aac04d644a6bd5e58
Eloise Holland <eloise.holland@Mac.Home>
2025-03-18 14:12:04 +0000

    creating instance of service in our class

--- test/services/LibraryServiceSpec.scala
+++ test/services/LibraryServiceSpec.scala
@@ -1,10 +1,16 @@
 package services
 
 import baseSpec.BaseSpec
+import connectors.LibraryConnector
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
+import scala.concurrent.ExecutionContext
+
 class LibraryServiceSpec extends BaseSpec with MockFactory with ScalaFutures with GuiceOneAppPerSuite {
+  val mockConnector: LibraryConnector = mock[LibraryConnector]
+  implicit val executionContext: ExecutionContext = app.injector.instanceOf[ExecutionContext]
+  val testService = new LibraryService(mockConnector)
 
 }
