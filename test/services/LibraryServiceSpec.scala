81fe93e12b9eb74b45d14bbfada13d70e9e8e694
Eloise Holland <eloise.holland@Mac.Home>
2025-03-18 14:11:06 +0000

    create LibraryServiceSpec

---
+++ test/services/LibraryServiceSpec.scala
@@ -0,0 +1,10 @@
+package services
+
+import baseSpec.BaseSpec
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+
+class LibraryServiceSpec extends BaseSpec with MockFactory with ScalaFutures with GuiceOneAppPerSuite {
+
+}
