0cb4ef3ffb325293495d5659a5645d6087a82412
Waris Hezb <waris.hezb@Mac.broadband>
2025-03-19 12:04:49 +0000

    update APIError to include NotFoundErrror and DatabaseError

--- app/models/APIError.scala
+++ app/models/APIError.scala
@@ -2,17 +2,25 @@ package models
 
 import play.api.http.Status
 
-sealed abstract class APIError(
-                                val httpResponseStatus: Int,
-                                val reason: String
-                              )
+sealed abstract class APIError(val httpResponseStatus: Int, val reason: String)
 
 object APIError {
 
   final case class BadAPIResponse(upstreamStatus: Int, upstreamMessage: String)
     extends APIError(
       Status.INTERNAL_SERVER_ERROR,
-      s"Bad response from upstream; got status: $upstreamStatus, and got reason $upstreamMessage"
+      s"Bad response from upstream; Status: $upstreamStatus, Reason: $upstreamMessage"
     )
 
+  final case class NotFoundError(details: String)
+    extends APIError(
+      Status.NOT_FOUND,
+      s"Resource not found: $details"
+    )
+
+  final case class DatabaseError(details: String)
+    extends APIError(
+      Status.INTERNAL_SERVER_ERROR,
+      s"Database operation failed: $details"
+    )
 }
