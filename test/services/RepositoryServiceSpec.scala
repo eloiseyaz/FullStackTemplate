515fc9f01fd8624c186c0b352ea44cc7bb95d206
Eloise Holland <eloise.holland@Eloises-MacBook-Air.local>
2025-03-20 12:23:48 +0000

    formatting for repository service tests

--- test/services/RepositoryServiceSpec.scala
+++ test/services/RepositoryServiceSpec.scala
@@ -1,5 +1,95 @@
 package services
 
-class RepositoryServiceSpec {
+import baseSpec.BaseSpecWithApplication
+import play.api.test.FakeRequest
+import repositories.DataRepository
+
+
+class RepositoryServiceSpec extends BaseSpecWithApplication {
+
+  val TestDataRepository = new DataRepository(???)(executionContext)
+
+  "RepositoryService .index" should {
+
+    "return books" in {
+      beforeEach()
+
+      val result = TestDataRepository.index()
+
+      result shouldBe ???
+
+      afterEach()
+    }
+
+    "retun 404 error" in {
+
+    }
+  }
+
+  "RepositoryService .create" should {
+
+    "create a book in the database" in {
+      beforeEach()
+
+
+      afterEach()
+    }
+
+    "return 500 error" in {
+      beforeEach()
+
+      afterEach()
+    }
+
+  }
+
+  "RepositoryService .read" should {
+
+    "find a book in the database by id" in {
+      beforeEach()
+
+      afterEach()
+    }
+
+    "return 404 NotFound for invalid ID" in {
+      beforeEach()
+
+
+      afterEach()
+    }
+
+  }
+
+  "RepositoryService .update" should {
+
+    "update a book in the database by id" in {
+      beforeEach()
+
+      afterEach()
+    }
+
+    "return 400 BadRequest if JSON is invalid" in {
+      beforeEach()
+
+      afterEach()
+    }
+
+  }
+
+  "RepositoryService .delete" should {
+
+    "delete a book in the database by id" in {
+      beforeEach()
+
+      afterEach()
+    }
+
+    "return 404 NotFound for invalid ID" in {
+      beforeEach()
+
+      afterEach()
+    }
+
+  }
 
 }
