775ec2bc86b66d95910b5045eb387c9913cb419f
Waris Hezb <waris.hezb@Mac.broadband>
2025-03-25 13:41:28 +0000

    testing for repositoryService

--- test/services/RepositoryServiceSpec.scala
+++ test/services/RepositoryServiceSpec.scala
@@ -69,7 +69,14 @@ class RepositoryServiceSpec extends BaseSpecWithApplication with MockFactory {
 
       "find a book in the database by id" in {
         beforeEach()
+        val createdBook = DataModel("1", "name", "author", "desc", 100)
+        (mockRepositoryService.read _)
+          .expects(*)
+          .returning(Future.successful(Right(DataModel("1", "name", "author", "desc", 100))))
 
+        whenReady(mockRepositoryService.read("111")) { result =>
+          result shouldBe Right(DataModel("1", "name", "author", "desc", 100))
+        }
 
 
         afterEach()
@@ -78,42 +85,90 @@ class RepositoryServiceSpec extends BaseSpecWithApplication with MockFactory {
       "return 404 NotFound for invalid ID" in {
         beforeEach()
 
+        (mockRepositoryService.read _)
+          .expects(*)
+          .returning(Future.successful(Left(APIError.BadAPIResponse(404, "Book ID not found"))))
+
+        whenReady(mockRepositoryService.read("111")) { result =>
+          result shouldBe Left(APIError.BadAPIResponse(404, "Book ID not found"))
+        }
 
         afterEach()
       }
 
     }
-  //
-  //  "RepositoryService .update" should {
-  //
-  //    "update a book in the database by id" in {
-  //      beforeEach()
-  //
-  //      afterEach()
-  //    }
-  //
-  //    "return 400 BadRequest if JSON is invalid" in {
-  //      beforeEach()
-  //
-  //      afterEach()
-  //    }
-  //
-  //  }
-  //
-  //  "RepositoryService .delete" should {
-  //
-  //    "delete a book in the database by id" in {
-  //      beforeEach()
-  //
-  //      afterEach()
-  //    }
-  //
-  //    "return 500 error for invalid ID" in {
-  //      beforeEach()
-  //
-  //      afterEach()
-  //    }
-  //
-  //  }
 
+    "RepositoryService .update" should {
+
+      "update a book in the database by id" in {
+        beforeEach()
+
+        val createdBook = DataModel("1", "name", "author", "desc", 100)
+        val updatedBook = DataModel("1", "ShadowSlave", "author", "desc", 100)
+        (mockRepositoryService.update _)
+          .expects(*, *)
+          .returning(Future.successful(Right(DataModel("1", "name", "author", "desc", 100))))
+
+        whenReady(mockRepositoryService.update("111", createdBook)) { result =>
+          result shouldBe Right(DataModel("1", "name", "author", "desc", 100))
+        }
+
+        afterEach()
+      }
+
+      "return 400 BadRequest if JSON is invalid" in {
+        beforeEach()
+        val createdBook = DataModel("1", "name", "author", "desc", 100)
+        (mockRepositoryService.update _)
+          .expects(*,*)
+          .returning(Future.successful(Left(APIError.BadAPIResponse(400, "JSON is invalid"))))
+
+        whenReady(mockRepositoryService.update("111", createdBook)) { result =>
+          result shouldBe Left(APIError.BadAPIResponse(400, "JSON is invalid"))
+        }
+
+        afterEach()
+      }
+
+    }
+
+    "RepositoryService .delete" should {
+
+      "delete a book in the database by id" in {
+        beforeEach()
+        (mockRepositoryService.delete _)
+          .expects(*)
+          .returning(Future.successful(Right("1234")))
+
+        whenReady(mockRepositoryService.delete("1234")) { result =>
+          result shouldBe Right("1234")
+        }
+        afterEach()
+      }
+
+      "return 404 error for invalid ID" in {
+        beforeEach()
+        (mockRepositoryService.delete _)
+          .expects(*)
+          .returning(Future.successful(Left(APIError.BadAPIResponse(404, "Book ID not found"))))
+
+        whenReady(mockRepositoryService.delete("111")) { result =>
+          result shouldBe Left(APIError.BadAPIResponse(404, "Book ID not found"))
+        }
+        afterEach()
+      }
+
+      "return 500 error failed to delete" in {
+        beforeEach()
+        (mockRepositoryService.delete _)
+          .expects(*)
+          .returning(Future.successful(Left(APIError.BadAPIResponse(500, "Failed to delete book"))))
+
+        whenReady(mockRepositoryService.delete("121")) { result =>
+          result shouldBe Left(APIError.BadAPIResponse(500, "Failed to delete book"))
+        }
+        afterEach()
+
+      }
+    }
 }
