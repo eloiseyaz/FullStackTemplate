542e5f1458c376a239e3c30be852c8c31c5ce1f9
tudorsonycx <tudorsonycx@gmail.com>
2025-03-26 12:30:01 +0000

    add NotFoundResponse case class to ApiError

--- app/models/APIError.scala
+++ app/models/APIError.scala
@@ -3,7 +3,7 @@ package models
 import play.api.http.Status
 import play.api.libs.json.{JsValue, Json, Writes}
 
-sealed abstract class APIError(val httpResponseStatus: Int, val reason: String)
+sealed abstract class APIError(val httpResponseStatus: Int, val reason: String) extends Exception
 
 object APIError {
 
@@ -12,6 +12,12 @@ object APIError {
       Status.INTERNAL_SERVER_ERROR,
       s"Bad response from upstream; Status: $upstreamStatus, Reason: $upstreamMessage"
     )
+
+  final case class NotFoundResponse(upstreamStatus: Int, upstreamMessage: String) extends APIError(
+    Status.NOT_FOUND,
+    s"Bad response from upstream; Status: $upstreamStatus, Reason: $upstreamMessage"
+  )
+
   implicit val badAPIResponseWrites: Writes[BadAPIResponse] = Json.writes[BadAPIResponse]
 
   implicit def eitherWrites[A: Writes, B: Writes]: Writes[Either[A, B]] = new Writes[Either[A, B]] {
